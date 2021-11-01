7b548f54e41d83389a5cefe80ed5c6214d9926d6
Ivan Mercier <ivan.mercier@gmail.com>
2021-11-01 21:47:48 +0100

    Generic data model

--- tools/create-tables.sql
+++ tools/create-tables.sql
@@ -1,17 +1,9 @@
-CREATE TABLE EnergyHistory
-(
-    "timestamp" timestamp NOT NULL PRIMARY key unique,
-    energyProduced numeric NOT NULL,
-    isValid boolean NOT NULL
-);
-CREATE TABLE sunEphemeride
-(
+CREATE TABLE sunEphemeride(
     day date NOT NULL PRIMARY key unique,
     sunrise timestamp NOT NULL,
     sunset timestamp NOT NULL,
     locationName varchar(32) NOT NULL
 );
-ALTER TABLE EnergyHistory add column isValid boolean;
 
 CREATE TABLE radiation("timestamp" timestamp NOT NULL PRIMARY key unique, radiation numeric NOT NULL);
 CREATE TABLE pression("timestamp" timestamp NOT NULL PRIMARY key unique, pression numeric NOT NULL);
@@ -29,32 +21,48 @@ CREATE TABLE sites
     latitude numeric not null,
     longitude numeric not null    
 );
-drop table sites;
-
 select * from sites;
-
-CREATE TABLE EnergyMetrics
-(
-    "timestamp" timestamp NOT NULL PRIMARY key unique,
-    energyProduced numeric NOT NULL,
-    isValid boolean NOT NULL
-);
 
 CREATE TABLE timeseries (
     siteId SERIAL NOT NULL,
     timestamp TIMESTAMP NOT NULL,
     sensor VARCHAR(128) NOT NULL,
-    value numeric NOT NULL
+    value numeric NOT NULL,
     PRIMARY KEY (siteId, sensor, timestamp)
 );
-drop table timeseries;
 CREATE INDEX ix_timeseries_sensor ON timeseries (sensor);
 CREATE INDEX ix_timeseries_timestamp ON timeseries (timestamp);
 CREATE INDEX ix_timeseries_siteid ON timeseries (siteid);
-truncate table timeseries;
+--truncate table timeseries;
 --delete from timeseries  where "timestamp" = '2021-10-31 10:45:00';
 --delete from timeseries  where siteid ='1723771';
 
 select * from timeseries order by "timestamp" desc;
-select * from timeseries where siteid ='2168697' order by "timestamp" desc;
 select * from timeseries where sensor ='energy' and siteid ='1723771' order by "timestamp" desc;
+select * from timeseries where siteid ='1723771' order by "timestamp" desc; --deviantion
+select * from timeseries where siteid ='2168697' order by "timestamp" desc; --boris
+
+insert into timeseries (siteId, timestamp, sensor, value)
+select 2168697, timestamp, 'energy', energyproduced from EnergyHistory
+ON CONFLICT(siteId, sensor, timestamp) DO UPDATE SET value = excluded.value;
+
+insert into timeseries (siteId, timestamp, sensor, value)
+select 2168697, timestamp, 'power', energyproduced*4 from EnergyHistory
+ON CONFLICT(siteId, sensor, timestamp) DO UPDATE SET value = excluded.value;
+
+CREATE TABLE ioWatt (
+	id BIGSERIAL primary KEY,
+	siteId INT NOT NULL,
+    execTime TIMESTAMP NOT NULL,
+    label VARCHAR NOT NULL,
+    apiVersion VARCHAR NOT NULL,    
+    data jsonb NOT null
+);
+CREATE UNIQUE INDEX index_iowatt_unicity ON ioWatt (siteId, label, (data->>'date'));
+--drop table iowatt;
+truncate table iowatt;
+
+select * from iowatt order by data->>'date' DESC;
+select id, exectime, data->>'date' as dateTime, data->>'inverterMode' as mode from iowatt order by id DESC;
+select data->>'date' as dateTime, data->>'value' from iowatt where label = 'power' order by data->>'date' DESC;
+select label, count(*) from iowatt group by label;
---
+++ tools/sh/version.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl https://monitoringapi.solaredge.com/version/supported
